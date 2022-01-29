0200d99c368a7e701d8ed15d1fddf7120f387b2f
imvik2000 <imvik2000@gmail.com>
2022-01-29 21:37:39 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class multiply {
+    public static void name() {
+       
+        int a,b,c;
+        a=10;
+        b=20;
+        c=a*b;
+        System.out.println("product  "+c); 
+    }
+}
