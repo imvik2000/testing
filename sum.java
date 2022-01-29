5ada9d0678dfabbed204ae0d415f9aaedfcd33dd
imvik2000 <imvik2000@gmail.com>
2022-01-29 21:05:02 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public Class sum
+{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        a=10;
+        b=20;
+        c=a+b;
+        System.out.println(c);
+        
+    }
+}
